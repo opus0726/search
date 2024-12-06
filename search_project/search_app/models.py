9b342f2ca4ffa58a3e10cb2ffa622adac907cc26
opus0726 <opus5595@gmail.com>
2024-12-06 16:17:39 +0900

    update

--- search_project/search_app/models.py
+++ search_project/search_app/models.py
@@ -10,14 +10,14 @@ class Product(models.Model):
     id = models.BigAutoField(primary_key=True) 
     name = models.CharField(max_length=255) 
     description = models.TextField() 
-    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1) 
  
     def __str__(self): 
         return self.name
     
     class Meta:
-        db_table = 'seach_db'
+        db_table = 'product'
 
 
 class Review(models.Model):
