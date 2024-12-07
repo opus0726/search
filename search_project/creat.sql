670c71d7881d885899ebba6cddd02e9c184ef44a
opus0726 <opus5595@gmail.com>
2024-12-07 09:37:39 +0900

    update

--- search_project/creat.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE book_book RENAME TO search_app_book;
-ALTER TABLE book_like RENAME TO search_app_like;
-ALTER TABLE book_review RENAME TO search_app_review;
-UPDATE django_content_type SET app_label='search_app' WHERE app_label='book';
-UPDATE django_migrations SET app='search_app' WHERE app='book';
